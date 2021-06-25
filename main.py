39e0acf2aa1dab481494816ffce6f6d15d173a07
e4exp <tomomi.daigou@gmail.com>
2021-06-25 04:56:47 +0000

    feature: 091_090_reformer_nocut_resnet18_fullatn_enc4dec2 trained

--- main.py
+++ main.py
@@ -69,8 +69,8 @@ def get_models(args):
 
     # parameters
     params = list(resnet.parameters())
-    #opt_resnet = torch.optim.Adam(params, lr=args.learning_rate)
-    opt_resnet = torch.optim.AdamW(params, lr=args.learning_rate)
+    opt_resnet = torch.optim.Adam(params, lr=args.learning_rate)
+    #opt_resnet = torch.optim.AdamW(params, lr=args.learning_rate)
     # set precision
     if args.fp16:
         resnet, opt_resnet = amp.initialize(models=resnet,
@@ -81,21 +81,21 @@ def get_models(args):
     # encoder
     encoder = Reformer(
         dim=args.dim_reformer,
-        depth=1,
+        depth=4,
         heads=1,
         max_seq_len=256,
         weight_tie=False,  # default=False
         use_full_attn=True,
-        ff_dropout=0.1,
-        post_attn_dropout=0.1,
+        #ff_dropout=0.1,
+        #post_attn_dropout=0.1,
     )
 
     encoder.to(args.device)
 
     # # parameters
     params = list(encoder.parameters())
-    #opt_encoder = torch.optim.Adam(params, lr=args.learning_rate)
-    opt_encoder = torch.optim.AdamW(params, lr=args.learning_rate)
+    opt_encoder = torch.optim.Adam(params, lr=args.learning_rate)
+    #opt_encoder = torch.optim.AdamW(params, lr=args.learning_rate)
     # set precision
     if args.fp16:
         encoder, opt_encoder = amp.initialize(models=encoder,
@@ -104,17 +104,18 @@ def get_models(args):
         amp._amp_state.loss_scalers[0]._loss_scale = 2**20
 
     # decoder
-    decoder = ReformerLM(num_tokens=args.vocab_size,
-                         dim=args.dim_reformer,
-                         depth=2,
-                         heads=1,
-                         max_seq_len=args.seq_len,
-                         weight_tie=False,
-                         weight_tie_embedding=False,
-                         use_full_attn=True,
-                         ff_dropout=0.1,
-                         post_attn_dropout=0.1,
-                         causal=True)
+    decoder = ReformerLM(
+        num_tokens=args.vocab_size,
+        dim=args.dim_reformer,
+        depth=2,
+        heads=1,
+        max_seq_len=args.seq_len,
+        weight_tie=False,
+        weight_tie_embedding=False,
+        use_full_attn=True,
+        #ff_dropout=0.1,
+        #post_attn_dropout=0.1,
+        causal=True)
     pad = args.vocab('__PAD__')
     decoder = TrainingWrapper(decoder, pad_value=pad)
 
@@ -122,8 +123,8 @@ def get_models(args):
 
     # parameters
     params = list(decoder.parameters())
-    #opt_decoder = torch.optim.Adam(params, lr=args.learning_rate)
-    opt_decoder = torch.optim.AdamW(params, lr=args.learning_rate)
+    opt_decoder = torch.optim.Adam(params, lr=args.learning_rate)
+    #opt_decoder = torch.optim.AdamW(params, lr=args.learning_rate)
     # set precision
     if args.fp16:
         decoder, opt_decoder = amp.initialize(models=decoder,
