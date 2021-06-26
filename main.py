51f5fdec79e17644ac44ef69be279308f5f58293
e4exp <tomomi.daigou@gmail.com>
2021-06-26 11:26:46 +0000

    feature: 096_095_reformer_nocut_resnet18_fullatn_enc2dec2_ehead2dhead2 trained

--- main.py
+++ main.py
@@ -81,8 +81,8 @@ def get_models(args):
     # encoder
     encoder = Reformer(
         dim=args.dim_reformer,
-        depth=4,
-        heads=1,
+        depth=2,
+        heads=2,
         max_seq_len=256,
         weight_tie=False,  # default=False
         use_full_attn=True,
@@ -107,8 +107,8 @@ def get_models(args):
     decoder = ReformerLM(
         num_tokens=args.vocab_size,
         dim=args.dim_reformer,
-        depth=4,
-        heads=1,
+        depth=2,
+        heads=2,
         max_seq_len=args.seq_len,
         weight_tie=False,
         weight_tie_embedding=False,
