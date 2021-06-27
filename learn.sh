2d954522dcd10ac7948cda11c1d396014ec0ab90
e4exp <tomomi.daigou@gmail.com>
2021-06-27 03:23:46 +0000

    feature: 097_096_reformer_nocut_resnet18_fullatn_enc2dec2_ehead4dhead4 tested

--- learn.sh
+++ learn.sh
@@ -6,7 +6,7 @@ data_name=014_flat_seq
 ckpt_name=ckpt
 #step_load=0
 #step_max=10000
-epoch_load=0
+epoch_load=10
 epoch_max=10
 batch_size=64
 batch_size_val=16
@@ -23,8 +23,8 @@ epoch_valid=5
 epoch_log=1
 
 
-mode=train
-#mode=test
+#mode=train
+mode=test
 #dbg=1
 dbg=0
 #log_level=DEBUG
