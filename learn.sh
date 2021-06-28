a455735336350b1205c425f499c7c3e9651f77c2
e4exp <tomomi.daigou@gmail.com>
2021-06-28 10:44:20 +0000

    feature: 099_098_reformer_nocut_resnet18_fullatn_enc3dec3_ehead4dhead4 trained

--- learn.sh
+++ learn.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
 code=main.py
-experiment_name=096_095_reformer_nocut_resnet18_fullatn_enc2dec2_ehead2dhead2
+experiment_name=099_098_reformer_nocut_resnet18_fullatn_enc3dec3_ehead4dhead4
 data_name=014_flat_seq
 ckpt_name=ckpt
 #step_load=0
 #step_max=10000
-epoch_load=10
+epoch_load=0
 epoch_max=10
 batch_size=64
 batch_size_val=16
-g_steps=8
+g_steps=16
 num_workers=4
 fp16_opt_level=O1
 max_grad_norm=1.0
@@ -23,8 +23,8 @@ epoch_valid=5
 epoch_log=1
 
 
-#mode=train
-mode=test
+mode=train
+#mode=test
 #dbg=1
 dbg=0
 #log_level=DEBUG
