689455970e419c097b85f0fc9045e7473384046d
e4exp <tomomi.daigou@gmail.com>
2021-06-29 05:18:17 +0000

    feature: 101_100_reformer_nocut_resnet18_fullatn_enc4dec4_ehead4dhead4 tested but something wrong

--- main.py
+++ main.py
@@ -82,7 +82,7 @@ def get_models(args):
     encoder = Reformer(
         dim=args.dim_reformer,
         depth=4,
-        heads=2,
+        heads=4,
         max_seq_len=256,
         weight_tie=False,  # default=False
         use_full_attn=True,
@@ -109,7 +109,7 @@ def get_models(args):
         num_tokens=args.vocab_size,
         dim=args.dim_reformer,
         depth=4,
-        heads=2,
+        heads=4,
         max_seq_len=args.seq_len,
         weight_tie=False,
         weight_tie_embedding=False,
