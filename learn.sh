83bb96144d21a8875425a3f9383e41fbccfde714
e4exp <tomomi.daigou@gmail.com>
2021-07-01 03:51:57 +0000

    feature: 103_102_reformer_nocut_resnet18_fullatn_enc4dec3_dhead4_adamw_drop01 tested

--- learn.sh
+++ learn.sh
@@ -6,7 +6,7 @@ data_name=014_flat_seq
 ckpt_name=ckpt
 #step_load=0
 #step_max=10000
-epoch_load=0
+epoch_load=10
 epoch_max=10
 batch_size=64
 batch_size_val=16
@@ -23,14 +23,14 @@ epoch_valid=5
 epoch_log=1
 
 
-mode=train
-#mode=test
+#mode=train
+mode=test
 #dbg=1
 dbg=0
 #log_level=DEBUG
 log_level=INFO
 log=${experiment_name}.log
-GPU=0
+GPU=1
 
 
 # debug
